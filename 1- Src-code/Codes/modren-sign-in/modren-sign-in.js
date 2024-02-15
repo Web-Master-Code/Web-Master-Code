72e44d351386ab69b102d09a1a28da0fb5bc7097
Abdalrhman-Mo-jaffar <157814489+Web-Master-Code@users.noreply.github.com>
2024-02-15 07:11:39 +0300

    Upload V.1.4

---
+++ 1- Src-code/Codes/modren-sign-in/modren-sign-in.js
@@ -0,0 +1,21 @@
+function checkUncheck1()
+{
+    var checkbox = document.getElementById('check1');
+    if (checkbox.checked) {
+        checkbox.checked = false;
+    }
+    else {
+        checkbox.checked = true
+    }
+}
+
+function checkUncheck2()
+{
+    var checkbox = document.getElementById('check2');
+    if (checkbox.checked) {
+        checkbox.checked = false;
+    }
+    else {
+        checkbox.checked = true
+    }
+}
